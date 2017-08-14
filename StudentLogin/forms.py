f9b6ea24c72db7c272c5bc3e8c7bc0714763784e
maddaladivya <maddaladivya3212@gmail.com>
2017-08-14 17:49:16 +0530

    Register CSS

--- StudentLogin/forms.py
+++ StudentLogin/forms.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(help_text='Enter your last name',
+                                widget=forms.TextInput(attrs={'placeholder': 'Username'}), required=False)
+    email = forms.EmailField(help_text='Enter your email id',
+                             widget=forms.EmailInput(attrs={'placeholder': 'Mail id'}), required=False)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), required=False)
 
     class Meta:
         model = User
